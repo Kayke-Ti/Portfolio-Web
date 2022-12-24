ad597436b87d57e0305f760ff5fb4416f86527ea
Kayke Barbosa <kaykeloiola@gmail.com>
2022-12-24 09:12:31 -0300

    10 commit

---
+++ Portifófio/src/components/AboutContainer.jsx
@@ -0,0 +1,17 @@
+const AboutContainer = () => {
+  return <section className="about-container">
+    <h2>Sobre</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      Magnam deserunt quasi ea tempore et incidunt, laboriosam 
+      ad neque a, eveniet doloremque harum hic nihil tempora, rem at? Laudantium, quod ab.
+      </p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+         Quibusdam, nisi quas, eum reprehenderit labore accusamus 
+         voluptate magni nostrum ut natus harum ratione nihil, commodi 
+         ad aspernatur inventore doloribus quis hic?
+      </p>
+  </section>
+}
+
+
+export default AboutContainer
---
+++ Portifófio/src/components/ProjectsContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const ProjectsContainer = () => {
+  return <section className="projects-container">
+    <h2>Projetos</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Odit, sint iste veritatis earum soluta explicabo repellat 
+      ullam exercitationem minus, vel at beatae non est? Exercitationem 
+      reprehenderit animi consequuntur ad aliquam?
+    </p>
+    <a href="" className="btn">
+      Ver Projetos
+    </a>
+  </section>
+
+}
+
+export default ProjectsContainer
